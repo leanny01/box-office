feb14b09d4420238468dba396653781103ca3bd2
leanny01 <dleannykalangu@gmail.com>
2020-07-20 11:51:16 +0200

    Added router

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -1,3 +1,11 @@
 import React from "react";
-import { connect } from "react-redux";
-import Presentation from "./Presentation.js";
+//import { connect } from "react-redux";
+//import Presentation from "./Presentation.js";
+
+const Home = () => {
+    return (
+        <div>Welcome to home page</div>
+    )
+}
+
+export default Home
